c448360f8b664da23ce96852660b487418f7c8df
Michael Jay Lupos <michaeljay.lupos@nodifi.com.au>
2022-08-08 15:39:32 +0800

    feat: added composition api example

--- pages/index.vue
+++ pages/index.vue
@@ -2,86 +2,123 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+        <v-card-text class="white--text">
+          <h1>Todo List({{totalTodos.length}})</h1>
+          <p><i>Using Composition API</i></p>
+          
+
+          <v-form>
+            <p v-if="isTyping" class="text--info">Typing...</p>
+            <v-text-field v-model="form.title" placeholder="Enter Title" :messages="'test'">
+            </v-text-field>
+
+            <v-textarea v-model="form.description" placeholder="Enter Description" :messages="'test'">
+            </v-textarea>
+            <v-btn @click="addItem">Add Item</v-btn>
+          </v-form>
+
+          <v-list>
+            <v-list-item v-for="(item, index) in totalTodos" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>{{item.title}}</v-list-item-title>
+                <v-list-item-subtitle>{{item.description}}</v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  <v-btn depressed outlined color="primary" @click="changeStatus(index, 'completed')">
+                    Mark as Completed
+                  </v-btn>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+       <v-card-text class="white--text">
+          <h1>Completed List({{totalCompleted.length}})</h1>
+          <p><i>Using Composition API</i></p>
+          
+          <v-list>
+            <v-list-item v-for="(item, index) in totalCompleted" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>{{item.title}}</v-list-item-title>
+                <v-list-item-subtitle>{{item.description}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { computed, onUnmounted, ref, watch } from 'vue'
+
+const existingItems = [
+  {title: 'Todo 1', description: 'Description 1', completed: false},
+  {title: 'Todo 2', description: 'Description 2', completed: false},
+  {title: 'Todo 3', description: 'Description 3', completed: false},
+]
+
 export default {
   name: 'IndexPage',
+  setup() {
+   /* 
+    [EXAMPLE]: Reactive data
+    [Further Read]: https://medium.com/@bsalwiczek/ref-vs-reactive-in-vue-3-whats-the-right-choice-7c6f7265ce39
+   */
+
+   const form = ref({title: '', description: '', completed: false})
+   const items = ref(existingItems)
+   const isTyping = ref(false)
+   let timeout = null
+
+  /* 
+    [EXAMPLE]: computed properties
+  */
+   
+   const totalTodos = computed(() => {
+    return items.value.filter(item => !item.completed)
+   })
+
+   const totalCompleted = computed(() => {
+    return items.value.filter(item => item.completed)
+   })
+
+    /* 
+      [EXAMPLE]: methods
+    */
+   const addItem = () => {
+     items.value.push(form)
+   }
+   const changeStatus = (index, status) => {
+     totalTodos.value[index].completed = status
+   }
+
+    /* 
+      [EXAMPLE]: watch with { deep:true }
+    */
+
+   const watchTyping = watch(form.value, ()=> {
+    clearInterval(timeout)
+    isTyping.value = !!(form.value.description || form.value.title)
+
+      timeout = setInterval(() => {
+        
+        isTyping.value = false
+      }, 800);
+    }, {deep: true})
+
+    /* 
+      [EXAMPLE]: setting watch off
+    */
+   onUnmounted(() => {
+    watchTyping()
+   })
+
+    return { form, items, totalTodos, totalCompleted, isTyping, addItem, changeStatus }
+  },
 }
 </script>
